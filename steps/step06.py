77ac99764c3cc448984eebcc7c520bccc518973a
Shoichiro Kawauchi <lacrosse91.ut@gmail.com>
2020-10-18 18:34:13 +0900

    ステップ6

---
+++ steps/step06.py
@@ -0,0 +1,68 @@
+import numpy as np
+
+class Variable:
+    def __init__(self, data):
+        """
+        初期化
+        """
+        self.data = data
+        self.grad = None
+
+
+class Function:
+    def __call__(self, input):
+        """
+        docstring
+        """
+        x = input.data # データを取り出す
+        y = self.forward(x) #　計算を別のメソッドで行う
+        output = Variable(y) # Variableに変換
+        self.input = input
+        return output
+
+    def forward(self, x):
+        """
+        docstring
+        """
+        raise NotImplementedError()
+
+    def backward(self, gy):
+        """
+        docstring
+        """
+        raise NotImplementedError()
+
+
+class Square(Function):
+    def forward(self, x):
+        return x ** 2
+
+    def backward(self, gy):
+        x = self.input.data
+        gx = 2 * x * gy
+        return gx
+
+class Exp(Function):
+    def forward(self, x):
+        return np.exp(x)
+
+    def backward(self, gy):
+        x = self.input.data
+        gx = np.exp(x) * gy
+        return gx
+
+
+A = Square()
+B = Exp()
+C = Square()
+
+x = Variable(np.array(0.5))
+a = A(x)
+b = B(a)
+y = C(b)
+
+y.grad = np.array(1.0)
+b.grad = C.backward(y.grad)
+a.grad = B.backward(b.grad)
+x.grad = A.backward(a.grad)
+print(x.grad)
