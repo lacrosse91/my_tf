073206f1cc9756aec0132672007d8d2c9cffe3f6
Shoichiro Kawauchi <lacrosse91.ut@gmail.com>
2020-10-21 10:37:03 +0900

    ステップ19

---
+++ steps/step19.py
@@ -0,0 +1,195 @@
+import numpy as np
+import unittest
+import weakref
+import contextlib
+
+@contextlib.contextmanager
+def using_config(name, value):
+    old_value = getattr(Config, name)
+    setattr(Config, name, value)
+    try:
+        yield
+    finally:
+        setattr(Config, name, old_value)
+
+def no_grad():
+    return using_config('enable_backprop', False)
+
+class Config(object):
+    """
+    docstring
+    """
+    enable_backprop = True
+
+class Variable:
+    def __init__(self, data, name = None):
+        """
+        ndarrayのみdataとして許可
+        """
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError('{} is not supported' .format(type(data)))
+
+        self.data = data
+        self.name = name
+        self.grad = None
+        self.creator = None
+        self.generation = 0
+
+    def __len__(self):
+        return len(self.data)
+
+    def __repr__(self):
+        if self.data is None:
+            return 'variable(None)'
+        p = str(self.data).replace('\n', '\n' + ' ' *  9)
+        return 'variable(' + p + ')'
+
+    @property
+    def shape(self):
+        return self.data.shape
+
+    @property
+    def size(self):
+        return self.data.size
+
+    @property
+    def dtype(self):
+        return self.data.dtype
+
+    def set_creator(self, func):
+        self.creator = func
+        self.generation = func.generation + 1
+
+    def cleargrad(self):
+        self.grad = None
+
+    def backward(self, retain_grad = False):
+
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+
+        funcs = []
+        seen_set = set()
+
+        def add_func(f):
+            if f not in seen_set:
+                funcs.append(f)
+                seen_set.add(f)
+                funcs.sort(key=lambda x: x.generation)
+
+        add_func(self.creator)
+
+        while funcs:
+            f = funcs.pop()
+            gys = [output().grad for output in f.outputs]
+            gxs = f.backward(*gys)
+            if not isinstance(gxs, tuple):
+                gxs = (gxs,)
+
+            for x, gx in zip(f.inputs, gxs):
+                if x.grad is None:
+                    x.grad = gx
+                else:
+                    x.grad = x.grad + gx
+
+                if x.creator is not None:
+                    add_func(x.creator)
+
+            if not retain_grad:
+                for y in f.outputs:
+                    y().grad = None
+
+
+class Function:
+    def __call__(self, *inputs):
+        """
+        docstring
+        """
+        xs = [x.data for x in inputs]
+        ys = self.forward(*xs) #　計算を別のメソッドで行う
+        if not isinstance(ys, tuple):
+            ys = (ys,)
+        outputs = [Variable(as_array(y)) for y in ys] # Variableに変換
+
+        if Config.enable_backprop:
+            self.generation = max([x.generation for x in inputs])
+            for output in outputs:
+                output.set_creator(self)
+            self.inputs = inputs
+            self.outputs = [weakref.ref(output) for output in outputs]
+
+        return outputs if len(outputs) > 1 else outputs[0]
+
+    def forward(self, xs):
+        """
+        docstring
+        """
+        raise NotImplementedError()
+
+    def backward(self, gys):
+        """
+        docstring
+        """
+        raise NotImplementedError()
+
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
+
+
+class Add(Function):
+    def forward(self, x0, x1):
+        y = x0 + x1
+        return y
+
+    def backward(self, gy):
+        return gy, gy
+
+def add(x0, x1):
+    return Add()(x0, x1)
+
+class Square(Function):
+    def forward(self, x):
+        return x ** 2
+
+    def backward(self, gy):
+        x = self.inputs[0].data
+        gx = 2 * x * gy
+        return gx
+
+def square(x):
+    return Square()(x)
+
+x = Variable(np.array([[2, 4], [3, 7]]))
+print(len(x))
+
+
+
+
+## テスト
+
+class AddTest(unittest.TestCase):
+
+    def test_forward(self):
+        x0 = Variable(np.array(2))
+        x1 = Variable(np.array(3))
+        y = add(x0, x1)
+        expected = np.array(5)
+        self.assertEqual(y.data, expected)
+
+class SquareTest(unittest.TestCase):
+
+    def test_forward(self):
+        x = Variable(np.array(2.0))
+        y = square(x)
+        expected = np.array(4.0)
+        self.assertEqual(y.data, expected)
+
+    def test_backward(self):
+        x = Variable(np.array(3.0))
+        y = square(x)
+        y.backward()
+        expected = np.array(6.0)
+        self.assertEqual(x.grad, expected)
